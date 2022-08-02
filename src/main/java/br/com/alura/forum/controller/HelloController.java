925504e9acbd247a1ded5ca01242b7bcb2303834
Gustavo Paes <gustavo910@gmail.com>
2022-08-02 15:40:32 -0300

    first commit

---
+++ src/main/java/br/com/alura/forum/controller/HelloController.java
@@ -0,0 +1,17 @@
+package br.com.alura.forum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/")
+	@ResponseBody
+	public String hello() {
+//		http://localhost:8080/
+		return "Hello World!";
+	}
+
+}
---
+++ src/main/java/br/com/alura/forum/controller/dto/TopicoDto.java
@@ -0,0 +1,54 @@
+package br.com.alura.forum.controller.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.com.alura.forum.modelo.Topico;
+
+public class TopicoDto {
+	
+	private Long id;
+	private String titulo;
+	private String mensagem;
+	private LocalDateTime dataCriacao;
+	
+	public TopicoDto(Topico topico) {
+
+		this.id= topico.getId();
+		this.titulo= topico.getTitulo();
+		this.mensagem= topico.getMensagem();
+		this.dataCriacao= topico.getDataCriacao();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public String getDuvida() {
+		return mensagem;
+	}
+	public LocalDateTime getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public static List<TopicoDto> converter(List<Topico> topicos) {
+		// TODO Auto-generated method stub
+//		List<TopicoDto> tdos= new ArrayList<TopicoDto>();
+//		topicos.forEach(t->{
+//			TopicoDto dto= new TopicoDto(t);
+//			tdos.add(dto);
+//		});
+//		return tdos;
+		return topicos.stream().map(TopicoDto::new).collect(Collectors.toList());
+		/* ele vai receber uma lista de topicos, vai passar pelo contrutor do topicoDto
+		 * que recebe um topico e cria um topicoDto e vai esta transformando isso em uma List.
+		 */
+		
+	}
+	
+	
+
+}
